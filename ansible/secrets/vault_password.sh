29912ea76a9125880ba5c10d76dc2504f96fda92
Mathias Pius <contact@pius.io>
2020-05-03 21:53:50 +0200

    Working versions of both sshd_config and users

--- ansible/secrets/vault_password.sh
+++ ansible/secrets/vault_password.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-gpg --batch --use-agent --decrypt "$ANSIBLE_RUNNING_DIR"secrets/vault_pass.gpg
+PKIPATH="${ANSIBLE_RUNNING_DIR:-$(dirname $(dirname $0))}/secrets"
+gpg --batch --use-agent --decrypt "$PKIPATH"/vault_pass.gpg
